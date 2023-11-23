515eb9631d10cef7af7bfe4d10acf022b4f02e7f
Alexander <kovgunovsasha7@gmail.com>
2023-11-23 21:02:22 +0300

    Initial commit

---
+++ src/main/java/edu/tms/zenflow/CourseProjectZenflowApplication.java
@@ -0,0 +1,13 @@
+package edu.tms.zenflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProjectZenflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProjectZenflowApplication.class, args);
+    }
+
+}
