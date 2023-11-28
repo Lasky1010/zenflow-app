3961c6c73c928b5937a92e749c481965a86918fe
Alexander <kovgunovsasha7@gmail.com>
2023-11-28 16:28:40 +0300

    ...

--- src/main/java/edu/tms/zenflow/mapper/UserMapper.java
+++ src/main/java/edu/tms/zenflow/mapper/UserMapper.java
@@ -1,9 +1,10 @@
 package edu.tms.zenflow.mapper;
 
-import edu.tms.zenflow.data.dto.UserDto;
+import edu.tms.zenflow.data.dto.user.UserDto;
 import edu.tms.zenflow.data.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 
